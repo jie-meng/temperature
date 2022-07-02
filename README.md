84567b252644c14a67839c45446692daa7088bea
Jie Meng <jmengxy@gmail.com>
2022-07-02 10:57:01 +0800

    Output xlsx

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 pip install -Ur requirements.txt
 
+## Prepare data
+
+Put images under __images/__. Add a related config file with the same name as image file but extension with 'txt'. Put the __ruler_min__ and __ruler_max__ value in it.
+
+Example:
+
+```
+ruler_min=20.2
+ruler_max=24.1
+```
+
 ## Execute
 
 python run.py
--- run.py
+++ run.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
 import re
 from dataclasses import dataclass
 from typing import List, Dict
+from pathlib import Path
 from PIL import Image
+from openpyxl import Workbook
+import numpy as np
+
 
 @dataclass
 class Data:
-    min_value: 0.0
-    max_value: 0.0
+    image_name: str = ''
+    point_count: int = 0
+    min_value: float = 0.0
+    max_value: float = 0.0
+    mean: float = 0.0
+    var: float = 0.0
+    std: float = 0.0
+    coefficient_of_variation: float = 0.0
+    data_array: List[float] = None
 
 
 def raw_diff(color1: List[float], color2: List[float]) -> float:
@@ -47,13 +57,13 @@ def calc_image_data(ruler: List[float], image: str, config: str) -> Data:
     ruler_min = float(config_data['ruler_min'])
     ruler_max = float(config_data['ruler_max'])
 
-    all_value_array = []
 
     target_im = Image.open(image)
     pix = target_im.load()
 
-    print('Calculating ...')
+    print('Calculating {0} ...'.format(image))
 
+    all_value_array = []
     for i in range(0, target_im.size[0]):
         for j in range(0, target_im.size[1]):
             # Only calculate non-transparent pixels
@@ -64,20 +74,67 @@ def calc_image_data(ruler: List[float], image: str, config: str) -> Data:
 
     target_im.close()
 
-    print('mean = {0}'.format(sum(all_value_array) / len(all_value_array)))
+    data = Data(
+        image_name = Path(image).stem,
+        point_count = len(all_value_array),
+        min_value = np.min(all_value_array),
+        max_value = np.max(all_value_array),
+        mean = np.mean(all_value_array),
+        var = np.var(all_value_array),
+        std = np.std(all_value_array),
+        data_array = all_value_array,
+    )
 
-    return None
+    data.coefficient_of_variation = data.std / data.mean
+
+    return data
+
+
+def write_output(data_array: List[Data]):
+    wb = Workbook()
+    ws1 = wb.active
+    ws1.title = '总体数据分析'
+    ws1['A1'] = '图片'
+    ws1['B1'] = '点数'
+    ws1['C1'] = '最小值'
+    ws1['D1'] = '最大值'
+    ws1['E1'] = '均值'
+    ws1['F1'] = '方差'
+    ws1['G1'] = '标准差'
+    ws1['H1'] = '变异系数'
+
+    ws2 = wb.create_sheet(title = '数据集')
+
+    for idx, data in enumerate(data_array):
+        ws1.cell(row = idx + 2, column = 1, value = data.image_name)
+        ws1.cell(row = idx + 2, column = 2, value = data.point_count)
+        ws1.cell(row = idx + 2, column = 3, value = data.min_value)
+        ws1.cell(row = idx + 2, column = 4, value = data.max_value)
+        ws1.cell(row = idx + 2, column = 5, value = data.mean)
+        ws1.cell(row = idx + 2, column = 6, value = data.var)
+        ws1.cell(row = idx + 2, column = 7, value = data.std)
+        ws1.cell(row = idx + 2, column = 8, value = data.coefficient_of_variation)
+
+        ws2.cell(row = 1, column = idx + 1, value = data.image_name) 
+        for data_idx, value in enumerate(data.data_array):
+            ws2.cell(row = data_idx + 2, column = idx + 1, value = value)
+
+    wb.save(filename = xlsx_file)
 
 
 if __name__ == '__main__':
+    # Find current script absolute path
     root_path = os.path.dirname(os.path.realpath(__file__))
 
+    # Load ruler
     ruler = load_ruler(root_path)
 
+    # Find images
     images = list(filter(lambda x: x.endswith('.png'), os.listdir('{0}/images'.format(root_path))))
     images.sort()
 
-    data_array = []
+    # Process each image
+    data_array: List[Data] = []
     for image in images:
         image_path = '{0}/images/{1}'.format(root_path, image)
         config_path = '{0}/images/{1}.txt'.format(root_path, os.path.splitext(image)[0])
@@ -88,3 +145,11 @@ if __name__ == '__main__':
 
         data_array.append(calc_image_data(ruler, image_path, config_path))
 
+    # Write xlsx
+    xlsx_file = '{0}/output/output.xlsx'.format(root_path)
+    print('Generate {0} ...'.format(xlsx_file))
+
+    if len(data_array) > 0:
+        write_output(data_array) 
+
+    print('Done!')
