86d563d5bb4d5bfafa0d90976d239657ac3732c9
Jie Meng <jmengxy@gmail.com>
2022-09-01 23:25:34 +0800

    Support sub folder of images path

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+from webbrowser import BaseBrowser
 import numpy as np
 from dataclasses import dataclass
 from typing import List, Dict
@@ -13,6 +14,7 @@ from openpyxl import Workbook
 @dataclass
 class Data:
     image_name: str = ''
+    is_ok: bool = False
     point_count: int = 0
     min_value: float = 0.0
     max_value: float = 0.0
@@ -86,40 +88,75 @@ def calc_image_data(ruler: List[float], image: str, config: str) -> Data:
     )
 
     data.coefficient_of_variation = data.std / data.mean
+    data.is_ok = True
 
     return data
 
 
-def write_output(data_array: List[Data]):
+def write_output(data_array: List[Data], xlsx_file: str):
     wb = Workbook()
     ws1 = wb.active
     ws1.title = '总体数据分析'
     ws1['A1'] = '图片'
-    ws1['B1'] = '点数'
-    ws1['C1'] = '最小值'
-    ws1['D1'] = '最大值'
-    ws1['E1'] = '均值'
-    ws1['F1'] = '方差'
-    ws1['G1'] = '标准差'
-    ws1['H1'] = '变异系数'
+    ws1['B1'] = '是否正常'
+    ws1['C1'] = '点数'
+    ws1['D1'] = '最小值'
+    ws1['E1'] = '最大值'
+    ws1['F1'] = '均值'
+    ws1['G1'] = '方差'
+    ws1['H1'] = '标准差'
+    ws1['I1'] = '变异系数'
 
     ws2 = wb.create_sheet(title = '数据集')
 
     for idx, data in enumerate(data_array):
         ws1.cell(row = idx + 2, column = 1, value = data.image_name)
-        ws1.cell(row = idx + 2, column = 2, value = data.point_count)
-        ws1.cell(row = idx + 2, column = 3, value = data.min_value)
-        ws1.cell(row = idx + 2, column = 4, value = data.max_value)
-        ws1.cell(row = idx + 2, column = 5, value = data.mean)
-        ws1.cell(row = idx + 2, column = 6, value = data.var)
-        ws1.cell(row = idx + 2, column = 7, value = data.std)
-        ws1.cell(row = idx + 2, column = 8, value = data.coefficient_of_variation)
+        ws1.cell(row = idx + 2, column = 2, value = data.is_ok)
+        ws1.cell(row = idx + 2, column = 3, value = data.point_count)
+        ws1.cell(row = idx + 2, column = 4, value = data.min_value)
+        ws1.cell(row = idx + 2, column = 5, value = data.max_value)
+        ws1.cell(row = idx + 2, column = 6, value = data.mean)
+        ws1.cell(row = idx + 2, column = 7, value = data.var)
+        ws1.cell(row = idx + 2, column = 8, value = data.std)
+        ws1.cell(row = idx + 2, column = 9, value = data.coefficient_of_variation)
 
-        ws2.cell(row = 1, column = idx + 1, value = data.image_name) 
+        ws2.cell(row = 1, column = idx + 1, value = data.image_name)
         for data_idx, value in enumerate(data.data_array):
             ws2.cell(row = data_idx + 2, column = idx + 1, value = value)
 
     wb.save(filename = xlsx_file)
+
+
+def process_collection(root_path: str, coll: str):
+    coll_path = '{0}/images/{1}'.format(root_path, coll)
+    print('\nProcessing collection {0} ...'.format(coll_path))
+
+    # Find images
+    images = list(filter(lambda x: x.endswith('.png'), os.listdir(coll_path)))
+    images.sort()
+
+    # Process each image
+    data_array: List[Data] = []
+    for image in images:
+        image_path = '{0}/{1}'.format(coll_path, image)
+        config_path = '{0}/{1}.txt'.format(coll_path, os.path.splitext(image)[0])
+
+        if not os.path.isfile(config_path):
+            print('Warning: cannot find {0}'.format(config_path))
+            continue
+
+        try:
+            data_array.append(calc_image_data(ruler, image_path, config_path))
+        except BaseException:
+            data_array.append(Data(image_name = Path(image).stem, is_ok = False, data_array = []))
+            continue
+
+    # Write xlsx
+    xlsx_file = '{0}/{1}.xlsx'.format(coll_path, coll)
+    print('Generate {0} ...'.format(xlsx_file))
+
+    if len(data_array) > 0:
+        write_output(data_array, xlsx_file)
 
 
 if __name__ == '__main__':
@@ -129,27 +166,12 @@ if __name__ == '__main__':
     # Load ruler
     ruler = load_ruler(root_path)
 
-    # Find images
-    images = list(filter(lambda x: x.endswith('.png'), os.listdir('{0}/images'.format(root_path))))
-    images.sort()
+    # Find collections
+    collections = list(filter(lambda x: os.path.isdir('{0}/images/{1}'.format(root_path, x)), os.listdir('{0}/images'.format(root_path))))
+    collections.sort()
 
-    # Process each image
-    data_array: List[Data] = []
-    for image in images:
-        image_path = '{0}/images/{1}'.format(root_path, image)
-        config_path = '{0}/images/{1}.txt'.format(root_path, os.path.splitext(image)[0])
+    for coll in collections:
+        process_collection(root_path, coll)
 
-        if not os.path.isfile(config_path):
-            print('Warning: cannot find {0}'.format(config_path))
-            continue
+    print('\nDone!')
 
-        data_array.append(calc_image_data(ruler, image_path, config_path))
-
-    # Write xlsx
-    xlsx_file = '{0}/output/output.xlsx'.format(root_path)
-    print('Generate {0} ...'.format(xlsx_file))
-
-    if len(data_array) > 0:
-        write_output(data_array) 
-
-    print('Done!')
